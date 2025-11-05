07920dfbc921f2f5055d1ee03c967507a4ca9fe9
Oleh Riepkin <alexandrov.oleg.ua@gmail.com>
2025-11-05 13:55:42 +0200

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:camera_test_task/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Camera test task',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
